f810cb8ddde3d422f09ed3b1a6bf2ad37e1ce1ef
nivas7920 <118712716+nivas7920@users.noreply.github.com>
2025-12-31 12:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+ const codes = document.querySelectorAll('.code');
+
+    codes[0].focus();
+
+    codes.forEach((input, index) => {
+
+        input.addEventListener('keydown', (e) => {
+
+            if (e.key === 'Backspace') {
+                if (input.value === '' && index > 0) {
+                    codes[index - 1].value = '';
+                    codes[index - 1].focus();
+                } else {
+                    input.value = '';
+                }
+            }
+        });
+
+        input.addEventListener('input', () => {
+            if (input.value && index < codes.length - 1) {
+                codes[index + 1].focus();
+            }
+        });
+
+    });
